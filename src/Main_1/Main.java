c148456fb81459f9469b4b8cc5f3ba0c96a2654c
codeants2012 <1713455269@qq.com>
2017-01-10 14:15:04 +0800

    ssss

--- src/Main_1/Main.java
+++ src/Main_1/Main.java
@@ -3,7 +3,8 @@ package Main_1;
 public class Main {
 
 	public static void main(String[] args) {
-		System.out.println("hello world!!!!");
+		Run s = new Run();
+		s.print("ssssssssssssssssaaaawweer");
 
 	}
 
